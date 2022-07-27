e1c4639203f2614ec15617a5e5039d2fac91dc17
Wetiba <erickw006@gmail.com>
2022-07-27 13:30:45 +0300

    2nd commit

--- app/src/main/java/com/example/sqlitedb2/MainActivity.kt
+++ app/src/main/java/com/example/sqlitedb2/MainActivity.kt
@@ -1,15 +1,17 @@
 package com.example.sqlitedb2
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("Range")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        printName.setOnClickListener {
+        addName.setOnClickListener {
             val db=DBHelper(this,null)
             val name=entername.text.toString()
             val age=enteragw.text.toString()
@@ -18,6 +20,21 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this,name+"added to db",Toast.LENGTH_LONG).show()
             entername.text.clear()
             enteragw.text.clear()
+
+        }
+        printName.setOnClickListener {
+            val db=DBHelper(this,null)
+
+            val cursor=db.getName()
+            cursor!!.moveToFirst()
+            Name.append(cursor.getString(cursor.getColumnIndex(DBHelper.NAME_COL))+ "\n")
+            Age.append(cursor.getString(cursor.getColumnIndex(DBHelper.AGE_COL))+ "\n")
+            while (cursor.moveToNext()){
+                Name.append(cursor.getString(cursor.getColumnIndex(DBHelper.NAME_COL))+ "\n")
+                Age.append(cursor.getString(cursor.getColumnIndex(DBHelper.AGE_COL))+ "\n")
+
+            }
+            cursor.close()
 
         }
     }
